4573d23daa905f135c2d511d3dc6519cacd31567
sharpstu <strt2k3@gmail.com>
2014-03-10 16:47:21 -0400

    VersusView class: method gameLoop()
    The VersusView class has a method called gameLoop() that will keep the
    game running. It is the main loop of the game.

---
+++ VersusView.java
@@ -0,0 +1,84 @@
+package views;
+
+import interfaces.FatalView;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.JPanel;
+
+/**
+ * This is the actual game panel where rules, AI, collision detection,
+ * and other necessary details for the fighting game to function will be.
+ * 
+ * @author Marcos Davila
+ *
+ */
+@SuppressWarnings("serial")
+public class VersusView extends JPanel implements Runnable, FatalView {
+
+	private Image img;
+	private boolean isRunning;
+
+	/**
+	 * Sets up the view for the players to fight
+	 */
+	public VersusView(){
+		init();
+	}
+	
+	/**
+	 * Paints the stage background.
+	 * 
+	 * @param g a Graphics object to do the rendering
+	 */
+	@Override
+	public void paintComponent(Graphics g) {
+		g.drawImage(img, 0, 0, this);
+	}
+
+	@Override
+	public void startThreads() {
+		
+	}
+
+	@Override
+	public void stopThreads() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			init();
+			gameLoop();
+		} finally {
+			stopThreads();
+		}
+	}
+
+	/*
+	 * The main loop of the video game
+	 */
+	private void gameLoop() {
+		// TODO Auto-generated method stub
+		while (isRunning){
+			try {
+				init();
+				gameLoop();
+			} finally {
+				stopThreads();
+			}
+		}
+	}
+
+	// Sets up the view and starts threads for the game to run
+	private void init() {
+		// TODO: Get a better background image
+    	img = kernel.loadImage("stage background.gif");
+    	startThreads();
+	}
+
+}
